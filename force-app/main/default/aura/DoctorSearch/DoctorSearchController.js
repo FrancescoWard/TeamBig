adf4a866f9a52ebeb2afee19b9e24eefd09ba05d
bdawk28 <bdawk28@gmail.com>
2019-12-26 15:58:06 -0500

    Doctor Search Update

--- force-app/main/default/aura/DoctorSearch/DoctorSearchController.js
+++ force-app/main/default/aura/DoctorSearch/DoctorSearchController.js
@@ -8,9 +8,7 @@
         
         getSpecialityOptions.setCallback(this, function(response) {
             if(response.getState() === "SUCCESS") {
-                //console.log(response.getReturnValue());
                 component.set("v.specOptions", response.getReturnValue());
-                //component.set("v.specOptions", response.getReturnValue());
             }
         });
         
@@ -19,16 +17,35 @@
         
         getGenderOptions.setCallback(this, function(response) {
             if(response.getState() === "SUCCESS") {
-                //console.log(response.getReturnValue());
-                component.set("v.gendOptions", response.getReturnValue());
-                //component.set("v.specOptions", response.getReturnValue());
+                component.set("v.genderOptions", response.getReturnValue());
             }
         });
         
+        //Languages
+        let getLangOptions = component.get("c.getAvailableDoctorLanguageOptions");
+        
+        getLangOptions.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS") {
+                component.set("v.langOptions", response.getReturnValue());
+            }
+        });
         $A.enqueueAction(getSpecialityOptions);
         $A.enqueueAction(getGenderOptions);
-
-    
+        $A.enqueueAction(getLangOptions);
     },
-    
+
+    SortDoctor : function (component, event, helper) {
+        let specialty = component.find("specialtyId").get("v.value");
+        let gender = component.find("genderId").get("v.value");
+        let language = component.find("langId").get("v.value");
+        let getDoctors = component.get("c.getDoctors");
+
+        getDoctors.setParams({"specialty" : specialty, "gender" : gender, "language" : language});
+        getDoctors.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS") {
+                component.set("v.Docs", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(getDoctors);
+    }
 })
