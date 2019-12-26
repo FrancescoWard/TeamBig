e572a75e479c33722582234cc33d46f2eb87e3b7
meretribia <brady.achterberg@gmail.com>
2019-12-26 16:39:19 -0500

    Further updates to doctor lightning page
    Updated doctor lightning page to handle some bugs--it's fully functional now.  Attempted to add more functions to Data Factory but it didnt' go anywhere.  Added more test functionality.

--- force-app/main/default/aura/DoctorMessageModal/DoctorMessageModalController.js
+++ force-app/main/default/aura/DoctorMessageModal/DoctorMessageModalController.js
@@ -1,11 +1,8 @@
 ({
-	init : function(component, event, helper) 
-    {
-	},
     // Called when user pushes the "Accept/Reject" button
     sendMessage : function(component, event, helper) 
     {	var setEvent = component.getEvent("updateAppt");
-        setEvent.setParams(	{ "message" : component.get("v.drmsg"),
+        setEvent.setParams(	{ "message" : component.find("v.drmsg"),
                            	  "status" : component.get("v.appt_status") }
                           );
         setEvent.fire();
