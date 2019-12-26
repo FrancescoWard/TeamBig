95101b6ad44b41379019bd203f681fb27dbd6236
Francesco Ward <francesco@Francesco.local>
2019-12-26 10:41:20 -0500

    add specialty and gender select options

--- force-app/main/default/aura/DoctorSearch/DoctorSearchController.js
+++ force-app/main/default/aura/DoctorSearch/DoctorSearchController.js
@@ -1,15 +1,34 @@
 ({
-    doInit : function(component, event, helper) {
-        let getDocs = component.get("c.getDoctors");
+    //Doctor search controller
+    
+    init : function (component, event, helper) {
+    
+        //Specialty
+        let getSpecialityOptions = component.get("c.getAvailableDoctorSpecialtyOptions");
         
-        getDocs.setCallback(this, function(response) {
+        getSpecialityOptions.setCallback(this, function(response) {
             if(response.getState() === "SUCCESS") {
-                console.log(response.getReturnValue());
-                component.set("v.Docs", response.getReturnValue());
+                //console.log(response.getReturnValue());
+                component.set("v.specOptions", response.getReturnValue());
+                //component.set("v.specOptions", response.getReturnValue());
             }
         });
         
-        $A.enqueueAction(getDocs);
+        //Gender
+        let getGenderOptions = component.get("c.getAvailableDoctorGenderOptions");
         
-	},
+        getGenderOptions.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS") {
+                //console.log(response.getReturnValue());
+                component.set("v.gendOptions", response.getReturnValue());
+                //component.set("v.specOptions", response.getReturnValue());
+            }
+        });
+        
+        $A.enqueueAction(getSpecialityOptions);
+        $A.enqueueAction(getGenderOptions);
+
+    
+    },
+    
 })
