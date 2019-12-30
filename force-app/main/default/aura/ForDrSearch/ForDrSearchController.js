857c348222c7def888e968de9f874e78eb177a92
meretribia <brady.achterberg@gmail.com>
2019-12-30 03:14:21 -0500

    Updates to lightning pages etc
    Fixed up Customer experience so search functionality, etc works as expected.

--- force-app/main/default/aura/ForDrSearch/ForDrSearchController.js
+++ force-app/main/default/aura/ForDrSearch/ForDrSearchController.js
@@ -1,76 +1,12 @@
 ({
-
     //Doctor search controller
-    onInit: function (component, event, helper) {
-        var cols = [
-            {label: 'Full Name', fieldName: 'Name', type: 'text'},
-                {label: 'Speciality', fieldName: 'Speciality__c', type: 'Picklist'},
-                {label: 'Gender', fieldName: 'Gender__c', type: 'Picklist'},
-                {label: 'Language', fieldName: 'Langauges__c', type: 'Picklist'}, 
-                {
-                             	
-        'label': 'Action',
-                'type': 'button',
-                'typeAttributes': {
-        			iconName: 'action:approval',
-                    'label': 'Schedule',
-                    'name': 'view_details'}}
-            ];
-        
-     
-        
-        component.set( 'v.mycolumns', cols );
-       // component.set( 'v.Docs', event.getParam( 'contacts' ) );
-        
-        //Speciality
-        let getSpecialityOptions = component.get("c.getAvailableDoctorSpecialtyOptions");
-        
-        getSpecialityOptions.setCallback(this, function(response) {
-            if(response.getState() === "SUCCESS") {
-                component.set("v.specOptions", response.getReturnValue());
-            }
-        });
-        
-        //Gender
-        let getGenderOptions = component.get("c.getAvailableDoctorGenderOptions");
-        
-        getGenderOptions.setCallback(this, function(response) {
-            if(response.getState() === "SUCCESS") {
-                component.set("v.genderOptions", response.getReturnValue());
-            }
-        });
-        
-        //Languages
-        let getLangOptions = component.get("c.getAvailableDoctorLanguageOptions");
-        
-        getLangOptions.setCallback(this, function(response) {
-            if(response.getState() === "SUCCESS") {
-                component.set("v.langOptions", response.getReturnValue());
-            }
-        
-        });
-        $A.enqueueAction(getSpecialityOptions);
-        $A.enqueueAction(getGenderOptions);
-        $A.enqueueAction(getLangOptions);
-       
-
+    onInit: function (component, event, helper) 
+    {	component.set( 'v.mycolumns', helper.setCols() );
+     	helper.loadPicklists(component);     
+     	helper.loadDrs(component);
     },
 
-    SortDoctor : function (component, event, helper) {
-        let specialty = component.find("specialtyId").get("v.value");
-        let gender = component.find("genderId").get("v.value");
-        let language = component.find("langId").get("v.value");
-        let getDoctors = component.get("c.getDoctors");
-       
-        //alert('Spec:= '+Specialty+'  Gender:='+Gender+'  Lang:='+Language); //+'   Doctors:='+ getDoctors+ '   id=: '+KeyField);
-        getDoctors.setParams({"specialty" : specialty, "gender" : gender, "language" : language});
-        getDoctors.setCallback(this, function(response) {
-            if(response.getState() === "SUCCESS") {
-                component.set("v.Docs", response.getReturnValue());
-            }
-        });
-        $A.enqueueAction(getDoctors);
-    },
+    SortDoctor : function (component, event, helper) { helper.loadDrs(component); },
 
     onRowAction : function ( component, event, helper ) {      
         
@@ -109,3 +45,16 @@
         }
     },
 })
+        /*let specialty = component.find("specialtyId").get("v.value");
+        let gender = component.find("genderId").get("v.value");
+        let language = component.find("langId").get("v.value");
+        let getDoctors = component.get("c.getDoctors");
+       
+        //alert('Spec:= '+Specialty+'  Gender:='+Gender+'  Lang:='+Language); //+'   Doctors:='+ getDoctors+ '   id=: '+KeyField);
+        getDoctors.setParams({"specialty" : specialty, "gender" : gender, "language" : language});
+        getDoctors.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS") {
+                component.set("v.Docs", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(getDoctors);*/
--- force-app/main/default/aura/ForDrSearch/ForDrSearchHelper.js
+++ force-app/main/default/aura/ForDrSearch/ForDrSearchHelper.js
@@ -1,10 +1,26 @@
 ({
-    //Doctor search controller
-    
-    init : function (component, event, helper) 
+    setCols : function()
+    {   const cols = 
+        [   {label: 'Full Name', fieldName: 'Name', type: 'text'},
+            {label: 'Speciality', fieldName: 'Speciality__c', type: 'Picklist'},
+            {label: 'Gender', fieldName: 'Gender__c', type: 'Picklist'},
+            {label: 'Language', fieldName: 'Languages__c', type: 'Picklist'}, 
+            {	'label': 'Action',
+                'type': 'button',
+                'typeAttributes': 
+             	{	iconName: 'action:approval',
+                    'label': 'Schedule',
+                    'name': 'view_details'
+                }
+            }		
+        ];
+     	return cols;
+    },
+    loadPicklists : function(component)
     {
-    	//Specialty
+        //Speciality
         let getSpecialityOptions = component.get("c.getAvailableDoctorSpecialtyOptions");
+        
         getSpecialityOptions.setCallback(this, function(response) {
             if(response.getState() === "SUCCESS") {
                 component.set("v.specOptions", response.getReturnValue());
@@ -13,6 +29,7 @@
         
         //Gender
         let getGenderOptions = component.get("c.getAvailableDoctorGenderOptions");
+        
         getGenderOptions.setCallback(this, function(response) {
             if(response.getState() === "SUCCESS") {
                 component.set("v.genderOptions", response.getReturnValue());
@@ -26,23 +43,23 @@
                 component.set("v.langOptions", response.getReturnValue());
             }
         });
+     
         $A.enqueueAction(getSpecialityOptions);
         $A.enqueueAction(getGenderOptions);
         $A.enqueueAction(getLangOptions);
     },
-
-    SortDoctor : function (component, event, helper) {
-        let specialty = component.find("specialtyId").get("v.value");
+	loadDrs : function(component) 
+    {	let specialty = component.find("specialtyId").get("v.value");
         let gender = component.find("genderId").get("v.value");
         let language = component.find("langId").get("v.value");
-        let getDoctors = component.get("c.getDoctors");
-
-        getDoctors.setParams({"specialty" : specialty, "gender" : gender, "language" : language});
-        getDoctors.setCallback(this, function(response) {
-            if(response.getState() === "SUCCESS") {
+     	console.log('Specialty: ' + specialty + 'Gender' + gender + 'Language' + language );
+     	// Controller Apex call
+     	let getDoctors = component.get("c.getDoctors");
+     	getDoctors.setParams( { "specialty" : specialty, "gender" : gender, "language" : language } );
+		getDoctors.setCallback	(this, function (response) 
+        {	if(response.getState() === "SUCCESS")
                 component.set("v.Docs", response.getReturnValue());
-            }
-        });
-        $A.enqueueAction(getDoctors);
-    }
+        }						);
+     	$A.enqueueAction(getDoctors);
+	}
 })
